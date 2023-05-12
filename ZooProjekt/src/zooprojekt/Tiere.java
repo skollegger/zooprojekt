43c9fc1fa98179932333107e4aab81a7c748ed40
Julia00005 <sju@gmx.net>
2023-05-12 08:33:25 +0200

    Tiere Properties eingefügt

--- ZooProjekt/src/zooprojekt/Tiere.java
+++ ZooProjekt/src/zooprojekt/Tiere.java
@@ -5,10 +5,23 @@
  */
 package zooprojekt;
 
+import java.util.Date;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
+
 /**
  *
  * @author julia
  */
 public class Tiere {
+    private int TiereId;
+    private StringProperty Name;
+    private StringProperty Art;
+    private IntegerProperty Alter;
+    private StringProperty Geschlecht;
+    private IntegerProperty Gehege;
+    private IntegerProperty Anzahl;
+    private Date Futterzeit;
+    
     
 }
